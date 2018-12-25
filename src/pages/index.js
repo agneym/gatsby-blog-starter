127b9355f8a44d13b8e736e662cbd2db03996098
BoyWithSilverWings <agney@outlook.in>
2018-12-25 11:24:50 +0530

    add create pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
 const BlogIndex = () => (
   <Layout>
     <SEO title="All Posts" keywords={[`gatsby`, `blog`, `react`]} />
+    <header>
+      <h1>Blog</h1>
+    </header>
   </Layout>
 );
 
 export default BlogIndex;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`;
---
+++ src/templates/blog-post.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark;
+    const siteTitle = this.props.data.site.siteMetadata.title;
+    const { previous, next } = this.props.pageContext;
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title={post.frontmatter.title} description={post.excerpt} />
+        <h1>{post.frontmatter.title}</h1>
+        <p>{post.frontmatter.date}</p>
+        <div
+          style={{
+            fontFamily: `Poppins, sans-serif`,
+          }}
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+        <hr />
+        <Bio />
+
+        <ul>
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </Layout>
+    );
+  }
+}
+
+export default BlogPostTemplate;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`;
