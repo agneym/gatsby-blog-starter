86d34c35281b4c6f30308a183726cffdc222efca
BoyWithSilverWings <agney@outlook.in>
2018-12-25 10:56:24 +0530

    remove header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Header from './header'
-import './layout.css'
+import './layout.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,7 +17,6 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
             margin: `0 auto`,
@@ -32,10 +30,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,10 @@ import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
+const BlogIndex = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SEO title="All Posts" keywords={[`gatsby`, `blog`, `react`]} />
   </Layout>
 );
 
-export default IndexPage;
+export default BlogIndex;
