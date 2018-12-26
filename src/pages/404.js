259ca26fb3e8e7b7f08b3160f11948147abb5deb
BoyWithSilverWings <agney@outlook.in>
2018-12-26 13:31:26 +0530

    Added custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Smiley = styled.span`
+  font-size: 10rem;
+  display: block;
+  margin: 20vmin 0;
+`;
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <Smiley role="img" aria-label="facepalm">
+        🤦🏻‍♂️
+      </Smiley>
+      <h1>NOT FOUND</h1>
+      <p>I have not added this page yet.</p>
+    </Container>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
